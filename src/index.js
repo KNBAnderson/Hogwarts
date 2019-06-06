53c106e6e3a68927465bb54227ae56b23795b401
katlin anderson and kaya jepson <katlin@>
2019-06-06 11:54:59 -0700

    try to update background color

--- src/index.js
+++ src/index.js
@@ -1,13 +1,43 @@
 import {Character} from './character';
 import $ from 'jquery';
 import './styles.css';
+import { classBody } from '@babel/types';
 
 $(document).ready(function() {
   let me = new Character();
-  // me.populateSpells();
-  $('#sortButton').click(function() {
+  $('.currentYear').text(me.year);
+  let promise = new Promise(function(resolve, reject) {
     me.populateClass();
-    $('#beforeSorting').hide();
-    $('#afterSorting').show();
+  });
+
+  $('#sortButton').click(function() {
+    promise
+      .then(response => { 
+        if(me.house === "Gryffindor") {
+          $('body').addClass("red");
+          console.log("red");
+        } else if (me.house === "Hufflepuff") {
+          $('body').addClass("yellow");
+          console.log("yellow");
+
+        } else if (me.house === "Slytherin") {
+          $('body').addClass("green");
+          console.log("green");
+
+        } else if (me.house === "Ravenclaw") {
+          $('body').addClass("blue");
+          console.log("blue");
+          
+        } else {
+          console.log("asyncs got ya down");
+        }
+        $('#beforeSorting').hide();
+        $('#afterSorting').show();
+      })
   })
+    
+
+  $('#viewClassmates').on('click', function() {
+    $('#classList').toggle();
+  });
 });
