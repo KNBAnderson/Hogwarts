17cdcc672adebab89e2ec99e052a9dd6983106a0
katlin anderson and kaya jepson <katlin@>
2019-06-06 12:06:28 -0700

    create working addClass functionality

--- src/index.js
+++ src/index.js
@@ -6,36 +6,35 @@ import { classBody } from '@babel/types';
 $(document).ready(function() {
   let me = new Character();
   $('.currentYear').text(me.year);
-  let promise = new Promise(function(resolve, reject) {
-    me.populateClass();
-  });
+  
 
-  $('#sortButton').click(function() {
-    promise
-      .then(response => { 
-        if(me.house === "Gryffindor") {
-          $('body').addClass("red");
-          console.log("red");
-        } else if (me.house === "Hufflepuff") {
-          $('body').addClass("yellow");
-          console.log("yellow");
+  $('#sortButton').click(() => {
+    let promise = new Promise(function(resolve, reject) {
+      me.populateClass();
+    });
+    setTimeout(() => {
+      if(me.house === "Gryffindor") {
+        $('body').addClass("red");
+        console.log("red");
+      } else if (me.house === "Hufflepuff") {
+        $('body').addClass("yellow");
+        console.log("yellow");
 
-        } else if (me.house === "Slytherin") {
-          $('body').addClass("green");
-          console.log("green");
+      } else if (me.house === "Slytherin") {
+        $('body').addClass("green");
+        console.log("green");
 
-        } else if (me.house === "Ravenclaw") {
-          $('body').addClass("blue");
-          console.log("blue");
-          
-        } else {
-          console.log("asyncs got ya down");
-        }
-        $('#beforeSorting').hide();
-        $('#afterSorting').show();
-      })
-  })
-    
+      } else if (me.house === "Ravenclaw") {
+        $('body').addClass("blue");
+        console.log("blue");
+        
+      } else {
+        console.log("asyncs got ya down");
+      }
+      $('#beforeSorting').hide();
+      $('#afterSorting').show();
+    }, 1000)
+  }); 
 
   $('#viewClassmates').on('click', function() {
     $('#classList').toggle();
